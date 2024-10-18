3155bf6936b911d194561135a9b83e5e849406f3
Deepak46544 <dkumar88743@gmail.com>
2024-10-18 15:21:50 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @raj14754
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7437346079:AAEWJfRBsX0IsOfa_G8Vynx1F69GWcJDFPQ")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@raj14754")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5674869424")
+
+
+
+
+
+  
